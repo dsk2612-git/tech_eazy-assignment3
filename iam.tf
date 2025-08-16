7000f86aa30ec318a091b00ccb9f9c6dff2a1c78
Sai Krishna <saiaws2612@gmail.com>
2025-08-16 23:47:35 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -2,7 +2,7 @@
 # IAM Role 1: Read-only on S3
 ##############################
 resource "aws_iam_role" "s3_readonly_role" {
-  name = "s3-readonly-role-${var.stage}"
+  name = "s3-readonly-role-${trimspace(var.stage)}"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -14,7 +14,7 @@ resource "aws_iam_role" "s3_readonly_role" {
 }
 
 resource "aws_iam_policy" "s3_readonly_policy" {
-  name = "s3-readonly-policy-${var.stage}"
+  name = "s3-readonly-policy-${trimspace(var.stage)}"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -31,7 +31,7 @@ resource "aws_iam_role_policy_attachment" "readonly_attach" {
 }
 
 resource "aws_iam_instance_profile" "readonly_instance_profile" {
-  name = "readonly-instance-profile-${var.stage}"
+  name = "readonly-instance-profile-${trimspace(var.stage)}"
   role = aws_iam_role.s3_readonly_role.name
 }
 
@@ -39,7 +39,7 @@ resource "aws_iam_instance_profile" "readonly_instance_profile" {
 # IAM Role 2: Full access for logs
 ##############################
 resource "aws_iam_role" "s3_fullaccess_role" {
-  name = "s3-fullaccess-role-${var.stage}"
+  name = "s3-fullaccess-role-${trimspace(var.stage)}"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -51,7 +51,7 @@ resource "aws_iam_role" "s3_fullaccess_role" {
 }
 
 resource "aws_iam_policy" "s3_fullaccess_policy" {
-  name = "s3-fullaccess-policy-${var.stage}"
+  name = "s3-fullaccess-policy-${trimspace(var.stage)}"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
